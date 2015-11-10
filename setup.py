949e8b88a774dc4de30aaff691a1f9f015ff494f
Richard Jones <richard@cottagelabs.com>
2015-11-10 16:18:05 +0000

    prep for dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'YOURAPP',
+    name = 'demo-edges',
     version = '1.0.0',
     packages = find_packages(),
     install_requires = [
@@ -12,7 +12,7 @@ setup(
     url = 'http://cottagelabs.com/',
     author = 'Cottage Labs',
     author_email = 'us@cottagelabs.com',
-    description = 'DESCRIBE YOUR APP',
+    description = 'scripts for hacking around with edges demos',
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
